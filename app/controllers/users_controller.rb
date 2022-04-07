c84e2a9165e16fe29cb4104374ff14d19989a2c4
Roylu7384 <roylu7384@gmail.com>
2022-04-07 11:24:58 -0400

    Add private method to sanitize input from signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,10 @@ class UsersController < ApplicationController
   def create
   end
 
+  private 
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+  
 end
