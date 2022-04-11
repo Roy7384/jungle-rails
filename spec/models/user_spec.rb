7aa1a9829ab6326a31f6473e96bb5a9b013459e5
Roylu7384 <roylu7384@gmail.com>
2022-04-11 15:23:48 -0400

    Setup user_spec file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  subject { described_class.new } 
+
+  it "should let me see what is going on" do
+    puts subject.inspect 
+  end
   
 end
