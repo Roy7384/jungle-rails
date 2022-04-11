cbfa65debb59487e82e099895e60b8eaf8e90417
Roylu7384 <roylu7384@gmail.com>
2022-04-11 17:15:57 -0400

    Implement authentication with wrong case emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates_length_of :password, :minimum => 6
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email.strip)
+    user = User.find_by('email ILIKE ?', email.strip)
     if (user && user.authenticate(password))
       return user
     else
