e2c507904b1b29da31f64d795a1a39df10790217
Roylu7384 <roylu7384@gmail.com>
2022-04-11 14:31:46 -0400

    Setup first validation test with all four mandatory fields given

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,21 +3,26 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   before(:each) do
     @category = Category.create(name: "Toy")
+    @product_params = {
+      :name => 'Monkey',
+      :price => 100,
+      :quantity => 20
+    }
   end
   
   describe 'Validations' do
 
     it "should save a product when all four mandatory fields are given" do
       puts @category.id
-      product_params = {
-        :name => 'Monkey',
-        :price => 100,
-        :quantity => 20
-      }
+      product_params = @product_params.clone
       product = @category.products.new(product_params)
       expect(product.save).to be true
     end
     
+    # it "should not save a product when name is missing" do
+    #   puts @category.id
+    #   product_params
+
   end
   
 end
