bfc4fcb02c642cdadc40b55f1b8e0d615d3c9a60
Roylu7384 <roylu7384@gmail.com>
2022-04-11 15:36:37 -0400

    Test for user not being validated when password or password_confirmation is not provided

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
   
+  it "should not be valid when password or password_confirmation is empty" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+    subject.password = "1234"
+    expect(subject).to be_valid 
+    subject.password_confirmation = "4321"
+    expect(subject).to_not be_valid 
+  end
+  
 end
